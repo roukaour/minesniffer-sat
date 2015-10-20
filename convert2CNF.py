2b7a9aaf959c62559c8e2e27d286a1ce275938d2
Remy Oukaour <remy.oukaour@gmail.com>
2015-10-20 09:47:21 -0400

    Use a Board wrapper class over the 2D array

--- convert2CNF.py
+++ convert2CNF.py
@@ -1,11 +1,40 @@
 import sys
+from itertools import product
+
+class Board(object):
+	
+	def __init__(self, height, width):
+		self.height = height
+		self.width = width
+		self.board = [[None] * width for _ in xrange(height)]
+	
+	def __getitem__(self, pos):
+		i, j = pos
+		assert 0 <= i < self.height and 0 <= j < self.width
+		return self.board[i][j]
+	
+	def __setitem__(self, pos, value):
+		i, j = pos
+		assert 0 <= i < self.height and 0 <= j < self.width and -1 <= value <= 8
+		self.board[i][j] = value
+	
+	def neighbors(self, pos):
+		i, j = pos
+		assert 0 <= i < self.height and 0 <= j < self.width
+		for (di, dj) in product([-1, 0, 1], [-1, 0, 1]):
+			ni, nj = i + di, j + dj
+			if 0 <= ni < self.height and 0 <= nj < self.width:
+				yield self[ni, nj]
 
 def parse_file(filepath):
 	# read the layout file to the board array
 	with open(filepath, 'r') as fin:
 		height, width = (int(t) for t in fin.readline().strip().split())
-		board = [[-1 if t == 'X' else int(t)
-			for t in line.strip().split(',')] for line in fin]
+		board = Board(height, width)
+		for i in xrange(height):
+			tokens = fin.readline().strip().split(',')
+			j in xrange(width):
+				board[i, j] = -1 if tokens[j] == 'X' else int(tokens[j])
 	return board
 
 def convert2CNF(board, filepath):
