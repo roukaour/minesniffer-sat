afa63cc907ffadea9ba43298e63241accc60adf9
Remy Oukaour <remy.oukaour@gmail.com>
2015-10-21 23:24:54 -0400

    Use sets to partially reduce clauses

--- convert2CNF.py
+++ convert2CNF.py
@@ -1,5 +1,5 @@
 import sys
-import itertools
+from itertools import product, combinations
 
 class Board(object):
 
@@ -8,39 +8,38 @@ class Board(object):
 		self.height = height
 		self.width = width
 		self.board = [[None] * width for _ in xrange(height)]
-		self.vars = {}
+		self.num_vars = 0
 
 	def __getitem__(self, pos):
 		i, j = pos
 		assert 0 <= i < self.height and 0 <= j < self.width
-		return self.board[i][j]
+		return abs(self.board[i][j])
 
 	def __setitem__(self, pos, value):
 		i, j = pos
-		assert 0 <= i < self.height and 0 <= j < self.width and -1 <= value <= 8
+		assert 0 <= i < self.height and 0 <= j < self.width
+		assert 0 <= value <= 8 or -value <= self.num_vars
 		self.board[i][j] = value
 
 	def positions(self):
-		return itertools.product(xrange(self.height), xrange(self.width))
+		return product(xrange(self.height), xrange(self.width))
+
+	def is_var(self, pos):
+		i, j = pos
+		assert 0 <= i < self.height and 0 <= j < self.width
+		return self.board[i][j] < 0
 
 	def neighbors(self, pos):
 		i, j = pos
 		assert 0 <= i < self.height and 0 <= j < self.width
-		for (di, dj) in itertools.product([-1, 0, 1], [-1, 0, 1]):
+		for (di, dj) in product([-1, 0, 1], [-1, 0, 1]):
+			if not di and not dj: continue
 			ni, nj = i + di, j + dj
 			if 0 <= ni < self.height and 0 <= nj < self.width:
 				yield (ni, nj)
 
 	def neighbor_variables(self, pos):
-		return [self.vars[n] for n in self.neighbors(pos) if self[n] == -1]
-
-	def number_variables(self):
-		n = 1
-		for (i, pos) in enumerate(self.positions()):
-			assert self[pos] is not None
-			if self[pos] == -1:
-				self.vars[pos] = n
-				n += 1
+		return [self[n] for n in self.neighbors(pos) if self.is_var(n)]
 
 def parse_file(filepath):
 	with open(filepath, 'r') as fin:
@@ -49,40 +48,43 @@ def parse_file(filepath):
 		for i in xrange(height):
 			tokens = fin.readline().strip().split(',')
 			for j in xrange(width):
-				board[i, j] = -1 if tokens[j] == 'X' else int(tokens[j])
-	board.number_variables()
+				if tokens[j] == 'X':
+					board.num_vars += 1
+					board[i, j] = -board.num_vars
+				else:
+					board[i, j] = int(tokens[j])
 	return board
 
 def make_conjunctions(unknowns, num_mines):
 	all_mines = set(unknowns)
-	for mine_cells in itertools.combinations(unknowns, num_mines):
+	for mine_cells in combinations(unknowns, num_mines):
 		mine_cells = set(mine_cells)
 		safe_cells = {-v for v in all_mines - mine_cells}
 		yield list(mine_cells | safe_cells)
 
 def distribute(*conjunctions):
 	if not conjunctions:
-		yield []
+		yield frozenset([])
 	else:
 		first = conjunctions[0]
 		for term in first:
 			for disjunction in distribute(*conjunctions[1:]):
-				yield [term] + disjunction
+				yield frozenset([term]) | disjunction
 
 def convert2CNF(board, filepath):
 	# Prepare the CNF output file (comment with the board file name)
 	fout = open(filepath, 'w')
 	fout.write('c ' + board.filepath.replace('\n', ' ') + '\n')
 	# Derive clauses from each number on the board
-	clauses = []
+	clauses = set()
 	for pos in board.positions():
+		if board.is_var(pos): continue
 		num_mines = board[pos]
-		if num_mines == -1: continue
 		unknowns = board.neighbor_variables(pos)
 		assignments = make_conjunctions(unknowns, num_mines)
-		clauses.extend(distribute(*assignments))
+		clauses.update(distribute(*assignments))
 	# Write the clauses to the output file
-	fout.write('p cnf %d %d\n' % (len(board.vars), len(clauses)))
+	fout.write('p cnf %d %d\n' % (board.num_vars, len(clauses)))
 	for clause in clauses:
 		fout.write(' '.join(str(v) for v in clause) + ' 0\n')
 	fout.close()
@@ -94,22 +96,22 @@ def convert2CNF_efficient(board, filepath):
 	# Derive clauses from each number on the board
 	clauses = set()
 	for pos in board.positions():
+		if board.is_var(pos): continue
 		num_mines = board[pos]
-		if num_mines == -1: continue
-		unknowns = [n for n in board.neighbors(pos) if board[n] == -1]
+		unknowns = board.neighbor_variables(pos)
 		# pos has M neighboring mines and has K neighboring unknowns
 		# (M == num_mines, K = len(unknowns))
 		# Exactly M of the K unknowns are mines, and exactly K-M are safe
 		# So in any subset of M+1 unknowns, one must be safe
 		for safe_subset in combinations(unknowns, num_mines + 1):
-			clauses.add(frozenset(-board.vars[p] for p in safe_subset))
+			clauses.add(frozenset(-v for v in safe_subset))
 		# And in any subset of K-M+1 unknowns, one must be a mine
 		for mine_subset in combinations(unknowns, len(unknowns) - num_mines + 1):
-			clauses.add(frozenset(board.vars[p] for p in mine_subset))
+			clauses.add(frozenset(mine_subset))
 	# Prune clauses which are weaker than others (e.g. A|B|C is weaker than A|B)
 	clauses = {c for c in clauses if not any(s for s in clauses if s != c and s.issubset(c))}
 	# Write the clauses to the output file
-	fout.write('p cnf %d %d\n' % (len(board.vars), len(clauses)))
+	fout.write('p cnf %d %d\n' % (board.num_vars, len(clauses)))
 	for clause in clauses:
 		fout.write(' '.join(str(v) for v in clause) + ' 0\n')
 	fout.close()
