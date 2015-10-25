c18aacdd2a841e048d3ebc2533ccdb79776f25c8
Remy Oukaour <remy.oukaour@gmail.com>
2015-10-24 23:21:37 -0400

    Clarify output format

--- prolog2.pl
+++ prolog2.pl
@@ -1,7 +1,21 @@
 % II.2 - The Assembly Problem
 
-% TODO
+% TODO:
 
+% Output should be something like this:
+/*
+topleft goes to center
+top goes to bottomright
+topright goes to bottom
+left goes to right
+center goes to top
+right goes to topleft
+bottomleft goes to left
+bottom goes to topright
+bottomright goes to bottomleft
+*/
+
+% Original positions:
 /*
 +------+  +------+  +------+
 |  -b  |  |  +a  |  |  -c  |
