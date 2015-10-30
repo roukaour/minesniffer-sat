c9f59b38615ba5baf55ac6663165611a289f85dc
Remy Oukaour <remy.oukaour@gmail.com>
2015-10-30 16:42:10 -0400

    Unsure about Prolog #2 output format

--- prolog2.pl
+++ prolog2.pl
@@ -7,28 +7,32 @@
 topleft goes to center rotated 90
 top goes to bottomright rotated 0
 topright goes to bottom rotated 180
-left goes to right rotated 180
-center goes to top rotated 0
-right goes to topleft rotated 270
-bottomleft goes to left rotated 0
-bottom goes to topright rotated 0
-bottomright goes to bottomleft rotated 90
+...
+*/
+
+% Or maybe like this:
+/*
+1. [-a, -c, +a, +b]
+2. [+b, -a, -b, +c]
+3. [-a, -d, +c, +b]
+...
 */
 
 % Original positions:
 /*
+1         2         3
 +------+  +------+  +------+
 |  -b  |  |  +a  |  |  -c  |
 |-a  +c|  |-d  +d|  |-d  +b|
 |  +d  |  |  -c  |  |  +d  |
 +------+  +------+  +------+
-
+4         5         6
 +------+  +------+  +------+
 |  -d  |  |  +b  |  |  -a  |
 |-b  -c|  |+d  -c|  |+b  -d|
 |  +d  |  |  -a  |  |  +c  |
 +------+  +------+  +------+
-
+7         8         9
 +------+  +------+  +------+
 |  -b  |  |  -a  |  |  -b  |
 |-a  +c|  |+b  -c|  |-c  +a|
